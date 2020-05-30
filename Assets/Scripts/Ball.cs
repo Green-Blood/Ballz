2f86c751357b97d1f2729719bd8a959bead8a6a9
Green-Blod <bloodjey98@gmail.com>
2020-05-30 19:50:20 +0500

    Initial commit

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ball : MonoBehaviour
+{
+    #region Variables
+    // Bool to check if a ball is clicked
+    bool _isClicked;
+    GameManager theGameManager;
+    #endregion
+    private void Start()
+    {
+        // Gets GameManager
+        theGameManager = FindObjectOfType<GameManager>();
+    }
+
+    // Checks if a ball is clicked
+    private void OnMouseDown()
+    {
+       _isClicked = true;
+    }
+
+    // Checks if the ball is touching another ball with the same color, 
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        if(_isClicked)
+        {
+            if(other.gameObject.tag == gameObject.tag)
+            {
+                // If the ball is touching another ball with the same color, destroy it!
+                Destroy(gameObject);   
+                Destroy(other.gameObject);
+                
+            }
+        }  
+    }
+    // Updates the score onDestroy
+    private void OnDestroy()
+    {
+        theGameManager.updateScore();
+    }
+    
+    
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    #region Variables
+    // Types of balls
+    public GameObject[] ballz;
+    // Quantity of balls on the screen
+    public int ballzQuantity;
+    // Position of generation of balls
+    public float xRandPos;
+    public float yPos;
+    // Score text
+    public Text theScoreText;
+    // Score count
+    public int score;
+    
+    // Checks if score has changed
+    bool _scoreChanged;
+    
+    #endregion
+    private void Start()
+    {
+        // Instantiate balls with it's quantity and range
+        for(int i = 0; i < ballzQuantity; i++)
+        {
+            Instantiate(ballz[Random.Range(0, ballz.Length)], new Vector3( Random.Range(-xRandPos, xRandPos), Random.Range(6f, yPos), 0f), Quaternion.identity);
+        }
+       
+    }
+
+    // If score is changed update scoreText
+    private void Update()
+    {
+        if(_scoreChanged)
+        {
+            theScoreText.text = "Score: " + score;
+        }
+    }
+    // Updates score
+    public void updateScore()
+    {
+        score++;
+        _scoreChanged = true;
+    }
+    
+}
